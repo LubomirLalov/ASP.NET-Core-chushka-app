a33df89c9dcf6c796571bfd548293dfcb4784087
LubomirLalov <lubomirlalov5@gmail.com>
2022-03-01 03:03:00 -0800

    added price validation

---
+++ Data/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPChushka.Data
+{
+    
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,3)")]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public TypeFood Type { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
